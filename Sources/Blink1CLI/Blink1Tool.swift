0e37f444739e5dd7c21ccaf60c4332539ab5651d
Dr. Michael Lauer <mickey@vanille-media.de>
2026-08-22 13:22:15 +0200

    Prepare 1.0.1
    One version across library, tool and app, as before.

--- Sources/Blink1CLI/Blink1Tool.swift
+++ Sources/Blink1CLI/Blink1Tool.swift
@@ -26,7 +26,7 @@ struct Blink1Tool: ParsableCommand {
           1  error                 64  usage error
           2  no blink(1) found
         """,
-        version: "1.0.0",
+        version: "1.0.1",
         subcommands: [
             ListCommand.self,
             StatusCommand.self,
